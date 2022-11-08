e379b25065530acd4ca2ddbc939699230d261078
Lans12345 <olanalans12345@gmail.com>
2022-11-08 17:09:48 +0800

    WIP. book now page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:taxi_user/auth/login.dart';
+import 'package:taxi_user/screens/booking_screens/book_now.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: BookNowScreen(),
     );
   }
 }
--- lib/screens/booking_screens/book_now.dart
+++ lib/screens/booking_screens/book_now.dart
@@ -4,27 +4,179 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:taxi_user/widgets/appbar/normal_appbar.dart';
 import 'package:taxi_user/widgets/drawer/drawer_widget.dart';
+import 'package:taxi_user/widgets/text/text_bold.dart';
 import 'package:taxi_user/widgets/text/text_regular.dart';
 
-class BookNowScreen extends StatelessWidget {
-  final Completer<GoogleMapController> _controller = Completer();
-
+class BookNowScreen extends StatefulWidget {
   static const CameraPosition _camPosition = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
   @override
+  State<BookNowScreen> createState() => _BookNowScreenState();
+}
+
+class _BookNowScreenState extends State<BookNowScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> markers = {};
+
+  marker1() async {
+    Marker mark1 = Marker(
+        markerId: const MarkerId('mark1'),
+        infoWindow: InfoWindow(
+          onTap: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Container(
+                    height: 750,
+                    color: Colors.red[600],
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        height: double.infinity,
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextBold(
+                                text: 'Driver and Taxi Information',
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextRegular(
+                                  text: 'Name: Lance Olana',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextRegular(
+                                  text: 'Plate Number: 12345',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextRegular(
+                                  text: 'Color of Vehicle: White',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextRegular(
+                                  text: 'Model: Honda Civic 192',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              TextBold(
+                                text: 'Location and Routes',
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextRegular(
+                                  text: 'Current Location',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  child: Center(
+                                    child: TextBold(
+                                        text: 'Your current location',
+                                        fontSize: 12,
+                                        color: Colors.black),
+                                  ),
+                                  height: 40,
+                                  width: 200,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextRegular(
+                                  text: 'Destination Location',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  child: Center(
+                                    child: TextBold(
+                                        text: 'Cagayan De Oro City',
+                                        fontSize: 12,
+                                        color: Colors.black),
+                                  ),
+                                  height: 40,
+                                  width: 200,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                });
+          },
+          snippet: '09090104355',
+          title: 'Lance Olana',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+        position: const LatLng(37.42796133580664, -122.085749655962));
+
+    setState(() {
+      markers.add(mark1);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const DrawerWidget(),
-      appBar: NormalAppbar('Home', Colors.white),
+      appBar: NormalAppbar('Book Now', Colors.white),
       body: Stack(
         children: [
           GoogleMap(
+            markers: Set<Marker>.from(markers),
             mapType: MapType.normal,
-            initialCameraPosition: _camPosition,
+            initialCameraPosition: BookNowScreen._camPosition,
             onMapCreated: (GoogleMapController controller) {
+              marker1();
               _controller.complete(controller);
             },
           ),
