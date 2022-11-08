fbc245172123f345709bac0763a5726f5f1d0e51
Lans12345 <olanalans12345@gmail.com>
2022-11-08 17:51:33 +0800

    Add. book now page

--- lib/screens/booking_screens/book_now.dart
+++ lib/screens/booking_screens/book_now.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:taxi_user/widgets/appbar/normal_appbar.dart';
+import 'package:taxi_user/widgets/buttons/button_widget.dart';
 import 'package:taxi_user/widgets/drawer/drawer_widget.dart';
 import 'package:taxi_user/widgets/text/text_bold.dart';
 import 'package:taxi_user/widgets/text/text_regular.dart';
@@ -28,124 +29,163 @@ class _BookNowScreenState extends State<BookNowScreen> {
           onTap: () {
             showModalBottomSheet(
                 context: context,
+                enableDrag: true,
                 builder: (context) {
                   return Container(
                     height: 750,
                     color: Colors.red[600],
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        height: double.infinity,
-                        width: double.infinity,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              TextBold(
-                                text: 'Driver and Taxi Information',
-                                fontSize: 16,
-                                color: Colors.black,
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              TextRegular(
-                                  text: 'Name: Lance Olana',
-                                  fontSize: 14,
-                                  color: Colors.black),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              TextRegular(
-                                  text: 'Plate Number: 12345',
-                                  fontSize: 14,
-                                  color: Colors.black),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              TextRegular(
-                                  text: 'Color of Vehicle: White',
-                                  fontSize: 14,
-                                  color: Colors.black),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              TextRegular(
-                                  text: 'Model: Honda Civic 192',
-                                  fontSize: 14,
-                                  color: Colors.black),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              TextBold(
-                                text: 'Location and Routes',
-                                fontSize: 16,
-                                color: Colors.black,
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              TextRegular(
-                                  text: 'Current Location',
-                                  fontSize: 14,
-                                  color: Colors.black),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              GestureDetector(
-                                onTap: () {},
-                                child: Container(
-                                  child: Center(
-                                    child: TextBold(
-                                        text: 'Your current location',
-                                        fontSize: 12,
-                                        color: Colors.black),
-                                  ),
-                                  height: 40,
-                                  width: 200,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    border: Border.all(
-                                      color: Colors.black,
+                      child: SingleChildScrollView(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          height: 520,
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                TextBold(
+                                  text: 'Driver and Taxi Information',
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextRegular(
+                                    text: 'Name: Lance Olana',
+                                    fontSize: 14,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextRegular(
+                                    text: 'Rating: 4.5 ★',
+                                    fontSize: 14,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextRegular(
+                                    text: 'Plate Number: 12345',
+                                    fontSize: 14,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextRegular(
+                                    text: 'Color of Vehicle: White',
+                                    fontSize: 14,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextRegular(
+                                    text: 'Model: Honda Civic 192',
+                                    fontSize: 14,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                TextBold(
+                                  text: 'Location and Routes',
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextRegular(
+                                    text: 'Current Location',
+                                    fontSize: 14,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Container(
+                                    child: Center(
+                                      child: TextBold(
+                                          text: 'Your current location',
+                                          fontSize: 12,
+                                          color: Colors.white),
+                                    ),
+                                    height: 40,
+                                    width: 200,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              TextRegular(
-                                  text: 'Destination Location',
-                                  fontSize: 14,
-                                  color: Colors.black),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              GestureDetector(
-                                onTap: () {},
-                                child: Container(
-                                  child: Center(
-                                    child: TextBold(
-                                        text: 'Cagayan De Oro City',
-                                        fontSize: 12,
-                                        color: Colors.black),
-                                  ),
-                                  height: 40,
-                                  width: 200,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    border: Border.all(
-                                      color: Colors.black,
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextRegular(
+                                    text: 'Destination Location',
+                                    fontSize: 14,
+                                    color: Colors.black),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Container(
+                                    child: Center(
+                                      child: TextBold(
+                                          text: 'Cagayan De Oro City',
+                                          fontSize: 12,
+                                          color: Colors.white),
+                                    ),
+                                    height: 40,
+                                    width: 200,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                ListTile(
+                                  leading: TextRegular(
+                                      text: 'Estimated Fare',
+                                      fontSize: 14,
+                                      color: Colors.black),
+                                  trailing: TextBold(
+                                      text: '200.00php',
+                                      fontSize: 18,
+                                      color: Colors.black),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 30, right: 30),
+                                    child: ButtonWidget(
+                                      label: 'Book Now',
+                                      color: Colors.red[600]!,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
