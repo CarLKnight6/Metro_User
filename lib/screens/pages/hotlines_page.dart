48ad67d16f326a4e676e1b59af04a183d11ba732
Lans12345 <olanalans12345@gmail.com>
2022-11-10 20:05:11 +0800

    Add. hotline page

--- lib/screens/pages/hotlines_page.dart
+++ lib/screens/pages/hotlines_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_user/widgets/buttons/hotline_button.dart';
+
+import '../../widgets/appbar/normal_appbar.dart';
+import '../../widgets/drawer/drawer_widget.dart';
 
 class HotlinesPage extends StatelessWidget {
   const HotlinesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: const DrawerWidget(),
+      backgroundColor: Colors.grey[200],
+      appBar: NormalAppbar('Hotlines', Colors.grey[300]!),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            HotlineButton(
+                contactName: 'SCIVER IT Solutions',
+                contactNumber: '09090104355',
+                onPressed: () {}),
+            const SizedBox(
+              height: 20,
+            ),
+            HotlineButton(
+                contactName: 'Taxi Region II Operator',
+                contactNumber: '09090104355',
+                onPressed: () {}),
+            const SizedBox(
+              height: 20,
+            ),
+            HotlineButton(
+                contactName: 'Ilocos Police Hotline',
+                contactNumber: '09090104355',
+                onPressed: () {}),
+            const SizedBox(
+              height: 20,
+            ),
+            HotlineButton(
+                contactName: 'Ilocos Ambulance Hotline',
+                contactNumber: '09090104355',
+                onPressed: () {}),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/buttons/hotline_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import '../text/text_bold.dart';
+import '../text/text_regular.dart';
+
+class HotlineButton extends StatelessWidget {
+  late VoidCallback onPressed;
+
+  late String contactNumber;
+  late String contactName;
+
+  HotlineButton(
+      {required this.contactName,
+      required this.contactNumber,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+      child: Column(
+        children: [
+          MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+              ),
+              minWidth: 275,
+              height: 40,
+              color: Colors.red,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    const Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    TextBold(
+                      text: contactNumber,
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              onPressed: onPressed),
+          const SizedBox(
+            height: 10,
+          ),
+          TextRegular(text: contactName, fontSize: 16, color: Colors.black),
+        ],
+      ),
+    );
+  }
+}
