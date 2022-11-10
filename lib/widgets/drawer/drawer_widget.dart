eab1eb5cbc29c338e61c940c1fa37a1991272495
Lans12345 <olanalans12345@gmail.com>
2022-11-10 19:29:34 +0800

    WIP. history page

--- lib/widgets/drawer/drawer_widget.dart
+++ lib/widgets/drawer/drawer_widget.dart
@@ -4,6 +4,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:taxi_user/screens/booking_screens/advance_booking.dart';
 import 'package:taxi_user/screens/booking_screens/book_now.dart';
 import 'package:taxi_user/screens/home_screen.dart';
+import 'package:taxi_user/screens/pages/history_page.dart';
 import 'package:taxi_user/screens/pages/profile_screen.dart';
 import 'package:taxi_user/widgets/containers/booking_tile.dart';
 import 'package:taxi_user/widgets/containers/normal_tile.dart';
@@ -89,7 +90,10 @@ class _MyDrawerState extends State<DrawerWidget> {
               NormalTile(
                   icon: Icons.history,
                   label: 'Ride History',
-                  onTap: () {},
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => const HistoryPage()));
+                  },
                   tileColor: Colors.transparent),
               NormalTile(
                   icon: Icons.person,
