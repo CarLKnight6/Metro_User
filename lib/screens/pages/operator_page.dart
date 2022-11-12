48c17575fad1bc5f965172b1ad345c9dc7df8636
Lans12345 <olanalans12345@gmail.com>
2022-11-12 16:43:16 +0800

    Add. operators page

--- lib/screens/pages/operator_page.dart
+++ lib/screens/pages/operator_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_user/widgets/text/text_bold.dart';
+import 'package:taxi_user/widgets/text/text_regular.dart';
+
+import '../../widgets/appbar/normal_appbar.dart';
+import '../../widgets/drawer/drawer_widget.dart';
 
 class OperatorPage extends StatelessWidget {
   const OperatorPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: const DrawerWidget(),
+      backgroundColor: Colors.grey[200],
+      appBar: NormalAppbar('Operator', Colors.grey[300]!),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              TextBold(
+                  text: 'Taxi Region II', fontSize: 16, color: Colors.black),
+              TextRegular(text: 'Operator', fontSize: 14, color: Colors.black),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
