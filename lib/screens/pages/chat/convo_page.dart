b3895b4a5368fcaf3fe6af64a7e5882cc7c7fbd4
Lans12345 <olanalans12345@gmail.com>
2022-11-12 19:08:35 +0800

    Add. convo page

--- lib/screens/pages/chat/convo_page.dart
+++ lib/screens/pages/chat/convo_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:taxi_user/widgets/text/text_bold.dart';
-import 'package:taxi_user/widgets/textfields/normal_field.dart';
+import 'package:taxi_user/widgets/text/text_regular.dart';
 
 class ConvoPage extends StatelessWidget {
   final _messageController = TextEditingController();
 
+  late String message = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         foregroundColor: Colors.black,
         centerTitle: true,
@@ -23,8 +25,93 @@ class ConvoPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          NormalTextField(
-              controller: _messageController, label: 'Type a message. .')
+          Expanded(
+            child: SizedBox(
+              child: ListView.builder(itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 10, 10, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Row(
+                        children: [
+                          SingleChildScrollView(
+                            child: Container(
+                              child: Center(
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                                  child: TextRegular(
+                                      text:
+                                          'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
+                                      fontSize: 12,
+                                      color: Colors.white),
+                                ),
+                              ),
+                              width: 220,
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(100),
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 1.5,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const CircleAvatar(
+                            minRadius: 25,
+                            maxRadius: 25,
+                            backgroundColor: Colors.black,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 100),
+                        child: TextRegular(
+                            text: '2:35pm', fontSize: 12, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 0, right: 0),
+            child: ColoredBox(
+              color: Colors.white,
+              child: TextFormField(
+                textCapitalization: TextCapitalization.sentences,
+                onChanged: (_input) {
+                  message = _input;
+                },
+                controller: _messageController,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      if (message == '') {
+                      } else {
+                        print('e');
+                      }
+                    },
+                    icon: const Icon(Icons.send, color: Colors.blue),
+                  ),
+                  hintText: '     Type your message. .',
+                  hintStyle: const TextStyle(
+                    fontFamily: 'QRegular',
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
