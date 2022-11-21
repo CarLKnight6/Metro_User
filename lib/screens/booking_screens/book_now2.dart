3fa7d7c0957d01d35aff58f37c8c6b44e2e8ba67
Lans12345 <olanalans12345@gmail.com>
2022-11-21 10:24:22 +0800

    added shared plus

--- lib/screens/booking_screens/book_now2.dart
+++ lib/screens/booking_screens/book_now2.dart
@@ -1,19 +1,17 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:taxi_user/widgets/drawer/drawer_widget.dart';
 import 'package:taxi_user/widgets/markers/my_location_marker.dart';
 import 'package:taxi_user/widgets/text/text_bold.dart';
 import 'package:taxi_user/widgets/text/text_regular.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BookNow2 extends StatefulWidget {
-  static const CameraPosition _camPosition = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
   @override
   State<BookNow2> createState() => _HomeScreenState();
 }
@@ -27,174 +25,208 @@ class _HomeScreenState extends State<BookNow2> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const DrawerWidget(),
-      appBar: AppBar(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.grey[300],
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.share),
-            ),
-          ]),
-      body: Stack(
-        children: [
-          GoogleMap(
-            markers: markers,
-            mapType: MapType.normal,
-            initialCameraPosition: BookNow2._camPosition,
-            onMapCreated: (GoogleMapController controller) {
-              setState(() {
-                myLocationMarker(
-                    markers, context, 37.42796133580664, -122.085749655962);
-              });
-              _controller.complete(controller);
-            },
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 2.5),
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextBold(
-                            text: 'Arriving by 2:45pm',
-                            fontSize: 18,
-                            color: Colors.black),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        TextRegular(
-                            text: 'Driver on the way!',
-                            fontSize: 14,
-                            color: Colors.black),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Center(
-                          child: Image.asset('assets/images/Arrow 3.png'),
-                        ),
-                      ],
-                    ),
+    final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+        .collection('Bookings')
+        .doc(box.read('bookingId'))
+        .snapshots();
+    return StreamBuilder<Object>(
+        stream: userData,
+        builder: (context, snapshot) {
+          dynamic data = snapshot.data;
+          return Scaffold(
+            drawer: const DrawerWidget(),
+            appBar: AppBar(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.grey[300],
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      Share.share(
+                          'My Ride\nDriver: ${data['driverName']}\nVehicle: ${data['vehicleModel']} ${data['plateNumber']}Destination: ${data['userDestination']}Date: ${data['dateTime']}');
+                    },
+                    icon: const Icon(Icons.share),
                   ),
-                  height: 100,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
+                ]),
+            body: Stack(
+              children: [
+                GoogleMap(
+                  markers: markers,
+                  mapType: MapType.normal,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(data['userLat'], data['userLang']),
+                    zoom: 14.4746,
                   ),
+                  onMapCreated: (GoogleMapController controller) {
+                    setState(() {
+                      myLocationMarker(
+                          markers, context, data['userLat'], data['userLang']);
+                    });
+                    _controller.complete(controller);
+                  },
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 2.5, 10, 10),
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          leading: const CircleAvatar(
-                            minRadius: 25,
-                            maxRadius: 25,
-                            backgroundColor: Colors.black,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 2.5),
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextBold(
+                                  text: 'Arriving by 2:45pm',
+                                  fontSize: 18,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              TextRegular(
+                                  text: 'Driver on the way!',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Center(
+                                child: Image.asset('assets/images/Arrow 3.png'),
+                              ),
+                            ],
                           ),
-                          title: TextBold(
-                              text: 'Lance O. Olana',
-                              fontSize: 18,
-                              color: Colors.black),
-                          subtitle: TextBold(
-                              text: 'Honda 123456',
-                              fontSize: 12,
-                              color: Colors.grey),
                         ),
-                        const SizedBox(
-                          height: 5,
+                        height: 100,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              width: 240,
-                              height: 50,
-                              child: TextFormField(
-                                textCapitalization:
-                                    TextCapitalization.sentences,
-                                style: const TextStyle(
-                                    color: Colors.black,
-                                    fontFamily: 'Quicksand'),
-                                onChanged: (_input) {},
-                                decoration: InputDecoration(
-                                  suffixIcon: IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.send),
-                                  ),
-                                  prefixIcon: const Icon(
-                                    Icons.chat_rounded,
-                                    color: Colors.grey,
-                                  ),
-                                  fillColor: Colors.white,
-                                  filled: true,
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: const BorderSide(
-                                        width: 1, color: Colors.grey),
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: const BorderSide(
-                                        width: 1, color: Colors.grey),
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  hintText: 'Chat with your driver',
-                                  hintStyle: const TextStyle(
-                                    fontFamily: 'QRegular',
-                                    color: Colors.grey,
-                                    fontSize: 12.0,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                border: Border.all(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              child: IconButton(
-                                onPressed: () {},
-                                icon: const Icon(
-                                  Icons.phone_outlined,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
+                      ),
                     ),
-                  ),
-                  height: 150,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 2.5, 10, 10),
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                leading: const CircleAvatar(
+                                  minRadius: 25,
+                                  maxRadius: 25,
+                                  backgroundColor: Colors.black,
+                                ),
+                                title: TextBold(
+                                    text: data['driverName'],
+                                    fontSize: 18,
+                                    color: Colors.black),
+                                subtitle: TextBold(
+                                    text: data['vehicleModel'] +
+                                        ' ' +
+                                        data['plateNumber'],
+                                    fontSize: 12,
+                                    color: Colors.grey),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    width: 240,
+                                    height: 50,
+                                    child: TextFormField(
+                                      textCapitalization:
+                                          TextCapitalization.sentences,
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontFamily: 'Quicksand'),
+                                      onChanged: (_input) {},
+                                      decoration: InputDecoration(
+                                        suffixIcon: IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(Icons.send),
+                                        ),
+                                        prefixIcon: const Icon(
+                                          Icons.chat_rounded,
+                                          color: Colors.grey,
+                                        ),
+                                        fillColor: Colors.white,
+                                        filled: true,
+                                        enabledBorder: OutlineInputBorder(
+                                          borderSide: const BorderSide(
+                                              width: 1, color: Colors.grey),
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderSide: const BorderSide(
+                                              width: 1, color: Colors.grey),
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                        ),
+                                        hintText: 'Chat with your driver',
+                                        hintStyle: const TextStyle(
+                                          fontFamily: 'QRegular',
+                                          color: Colors.grey,
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      final _text =
+                                          "tel:${data['driverContactNumber']}";
+                                      if (await canLaunch(_text)) {
+                                        await launch(_text);
+                                      }
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                      child: IconButton(
+                                        onPressed: () async {
+                                          final _text =
+                                              "tel:${data['driverContactNumber']}";
+                                          if (await canLaunch(_text)) {
+                                            await launch(_text);
+                                          }
+                                        },
+                                        icon: const Icon(
+                                          Icons.phone_outlined,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        height: 150,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+              ],
+            ),
+          );
+        });
   }
 }
