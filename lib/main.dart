7d7de5ae6134c092b57790130ff2e5c0bd8af718
Lans12345 <olanalans12345@gmail.com>
2022-11-30 09:45:37 +0800

    added  saved locations

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:taxi_user/screens/home_screen.dart';
+import 'package:taxi_user/screens/booking_screens/book_now.dart';
 
 import 'auth/login.dart';
 import 'firebase_options.dart';
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return HomeScreen();
+              return BookNowScreen();
             } else {
               return LoginPage();
             }
--- lib/widgets/delegate/search_destination.dart
+++ lib/widgets/delegate/search_destination.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:get_storage/get_storage.dart';
 
 import 'package:taxi_user/services/locations/place_service.dart';
 import 'package:taxi_user/services/providers/destination_provider.dart';
+import 'package:taxi_user/widgets/text/text_regular.dart';
 
 class AddressSearch extends SearchDelegate<Suggestion> {
   AddressSearch(this.sessionToken) {
@@ -46,6 +51,22 @@ class AddressSearch extends SearchDelegate<Suggestion> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
+
+    showToast() {
+      Fluttertoast.showToast(
+          msg: "Invalid. Place is not set!",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+
     return FutureBuilder(
         future: query == ""
             ? null
@@ -53,10 +74,104 @@ class AddressSearch extends SearchDelegate<Suggestion> {
                 query, Localizations.localeOf(context).languageCode),
         builder: (context, AsyncSnapshot<List> snapshot) {
           return query == ''
-              ? Container(
-                  padding: const EdgeInsets.all(16.0),
-                  child: const Text('Search for Location'),
-                )
+              ? StreamBuilder<DocumentSnapshot>(
+                  stream: userData,
+                  builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(child: Text('Loading'));
+                    } else if (snapshot.hasError) {
+                      return const Center(child: Text('Something went wrong'));
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    dynamic data = snapshot.data;
+                    return Consumer(
+                      builder: ((context, ref, child) {
+                        return Column(
+                          children: [
+                            ListTile(
+                              onTap: () async {
+                                if (data['homeLat'] == 0) {
+                                  showToast();
+                                } else {
+                                  Navigator.of(context).pop();
+
+                                  List<Placemark> placemarks =
+                                      await placemarkFromCoordinates(
+                                          data['homeLat'], data['homeLong']);
+                                  print(placemarks);
+                                  Placemark place = placemarks[0];
+
+                                  ref.read(destinationProvider.notifier).state =
+                                      '${place.street}, ${place.locality} ';
+
+                                  print('${place.street}, ${place.locality} ');
+                                }
+                              },
+                              leading: TextRegular(
+                                  text: 'Home',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              trailing: const Icon(Icons.home),
+                            ),
+                            ListTile(
+                              onTap: () async {
+                                if (data['officeLat'] == 0) {
+                                  showToast();
+                                } else {
+                                  Navigator.of(context).pop();
+
+                                  List<Placemark> placemarks =
+                                      await placemarkFromCoordinates(
+                                          data['officeLat'],
+                                          data['officeLong']);
+                                  print(placemarks);
+                                  Placemark place = placemarks[0];
+
+                                  ref.read(destinationProvider.notifier).state =
+                                      '${place.street}, ${place.locality} ';
+
+                                  print('${place.street}, ${place.locality} ');
+                                }
+                              },
+                              leading: TextRegular(
+                                  text: 'Workplace',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              trailing: const Icon(Icons.work_outline),
+                            ),
+                            ListTile(
+                              onTap: () async {
+                                if (data['schoolLat'] == 0) {
+                                  showToast();
+                                } else {
+                                  Navigator.of(context).pop();
+
+                                  List<Placemark> placemarks =
+                                      await placemarkFromCoordinates(
+                                          data['schoolLat'],
+                                          data['schoolLong']);
+                                  print(placemarks);
+                                  Placemark place = placemarks[0];
+
+                                  ref.read(destinationProvider.notifier).state =
+                                      '${place.street}, ${place.locality} ';
+
+                                  print('${place.street}, ${place.locality} ');
+                                }
+                              },
+                              leading: TextRegular(
+                                  text: 'School',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              trailing: const Icon(Icons.school_outlined),
+                            ),
+                          ],
+                        );
+                      }),
+                    );
+                  })
               : snapshot.hasData
                   ? Consumer(
                       builder: ((context, ref, child) {
