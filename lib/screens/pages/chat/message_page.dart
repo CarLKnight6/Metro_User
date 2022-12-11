379563421d6b151484bbd4c18fd8352d5435f680
Lans12345 <olanalans12345@gmail.com>
2022-12-11 12:46:57 +0800

    wip fetching messages

--- lib/screens/pages/chat/message_page.dart
+++ lib/screens/pages/chat/message_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:taxi_user/screens/pages/chat/convo_page.dart';
 import 'package:taxi_user/widgets/appbar/normal_appbar.dart';
@@ -15,34 +17,58 @@ class MessagePage extends StatelessWidget {
       drawer: const DrawerWidget(),
       backgroundColor: Colors.grey[200],
       appBar: NormalAppbar('Messages', Colors.grey[300]!),
-      body: ListView.builder(
-        itemBuilder: ((context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => ConvoPage()));
-              },
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              tileColor: Colors.white,
-              leading: const CircleAvatar(
-                backgroundColor: Colors.black,
-                minRadius: 25,
-                maxRadius: 25,
-              ),
-              title: TextBold(
-                  text: 'Lance Olana', fontSize: 18, color: Colors.black),
-              subtitle: TextRegular(
-                  text: 'Rating: 5 ★', fontSize: 12, color: Colors.grey),
-              trailing: TextRegular(
-                  text: '2:45pm', fontSize: 14, color: Colors.black),
-            ),
-          );
-        }),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection(FirebaseAuth.instance.currentUser!.uid)
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              print('error');
+              return const Center(child: Text('Error'));
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              print('waiting');
+              return const Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.black,
+                )),
+              );
+            }
+
+            final data = snapshot.requireData;
+            return ListView.builder(
+              itemCount: snapshot.data?.size ?? 0,
+              itemBuilder: ((context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    onTap: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => ConvoPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    tileColor: Colors.white,
+                    leading: const CircleAvatar(
+                      backgroundColor: Colors.black,
+                      minRadius: 25,
+                      maxRadius: 25,
+                    ),
+                    title: TextBold(
+                        text: 'Lorem Ipsum', fontSize: 18, color: Colors.black),
+                    subtitle: TextRegular(
+                        text: 'Lance Olana', fontSize: 12, color: Colors.grey),
+                    trailing: TextRegular(
+                        text: '2:45pm', fontSize: 14, color: Colors.black),
+                  ),
+                );
+              }),
+            );
+          }),
     );
   }
 }
