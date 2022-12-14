309e7d3e05ea30d85760d20858ddf0095913d8f4
Lans12345 <olanalans12345@gmail.com>
2022-12-14 13:28:26 +0800

    added hotlines

--- lib/screens/pages/hotlines_page.dart
+++ lib/screens/pages/hotlines_page.dart
@@ -8,7 +8,85 @@ import '../../widgets/drawer/drawer_widget.dart';
 class HotlinesPage extends StatelessWidget {
   HotlinesPage({Key? key}) : super(key: key);
 
-  List hotlines = [];
+  List hotlines = [
+    'BFP Tuguegarao',
+    'CAGELCO',
+    'Command Center',
+    'CDRRMO',
+    'NDRRMC',
+    'PNP Tuguegarao',
+    'Rescue 111 1',
+    'TCPGH',
+    'TCTMG',
+    'Tuguegarao Information Office',
+    'PNP Abulug',
+    'PNP Alcala',
+    'PNP Allacapan',
+    'PNP Amulung',
+    'PNP Aparri',
+    'PNP Baggao',
+    'PNP Ballesteros',
+    'PNP Buguey',
+    'PNP Calayan',
+    'PNP Camalaniugan',
+    'PNP Claveria',
+    'PNP Enrile',
+    'PNP Gonzaga',
+    'PNP Gattaran',
+    'PNP Iguig',
+    'PNP Lal-lo',
+    'PNP Lasam',
+    'PNP Pamplona',
+    'PNP Penablanca',
+    'PNP Piat',
+    'PNP Rizal',
+    'PNP Sanchez Mira',
+    'PNP Solana',
+    'PNP Sta. Ana',
+    'PNP Sta. Prexedes',
+    'PNP Sta. Teresita',
+    'PNP Sto. Nino',
+  ];
+
+  List numbers = [
+    '09178113474',
+    '09175782437',
+    '09560697141',
+    '(078)3041714',
+    '9111406',
+    '09058005118',
+    '09066229924',
+    '0788441337',
+    '09265378090',
+    '0783771632',
+    '09066426468',
+    '09278602133',
+    '09068323322',
+    '09175720744',
+    '09172032003',
+    '09359869494',
+    '09277500799',
+    '09152299200',
+    '09461932489',
+    '09175146905',
+    '09174724084',
+    '09069723081',
+    '09178896216',
+    '09165177702',
+    '09175991247',
+    '09052011953',
+    '09057955955',
+    '09057969547',
+    '09261712844',
+    '09262968628',
+    '09162458090',
+    '09177722352',
+    '09175932574',
+    '09274352980',
+    '09177713861',
+    '09262591257',
+    '09063466951',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,57 +94,26 @@ class HotlinesPage extends StatelessWidget {
       drawer: const DrawerWidget(),
       backgroundColor: Colors.grey[200],
       appBar: NormalAppbar('Hotlines', Colors.grey[300]!),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            HotlineButton(
-                contactName: 'SCIVER IT Solutions',
-                contactNumber: '09980064774',
-                onPressed: () async {
-                  const _text = 'tel:09980064774';
-                  if (await canLaunch(_text)) {
-                    await launch(_text);
-                  }
-                }),
-            const SizedBox(
-              height: 20,
-            ),
-            HotlineButton(
-                contactName: 'Taxi Region II Operator',
-                contactNumber: '09213820660',
-                onPressed: () async {
-                  const _text = 'tel:09213820660';
-                  if (await canLaunch(_text)) {
-                    await launch(_text);
-                  }
-                }),
-            const SizedBox(
-              height: 20,
-            ),
-            HotlineButton(
-                contactName: 'Tuguegarao Police Hotline',
-                contactNumber: '09058005118',
-                onPressed: () async {
-                  const _text = 'tel:09058005118';
-                  if (await canLaunch(_text)) {
-                    await launch(_text);
-                  }
-                }),
-            const SizedBox(
-              height: 20,
-            ),
-            HotlineButton(
-                contactName: 'Tuguegarao Rescue',
-                contactNumber: '09066229924',
-                onPressed: () async {
-                  const _text = 'tel:09066229924';
-                  if (await canLaunch(_text)) {
-                    await launch(_text);
-                  }
-                }),
-          ],
-        ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, bottom: 20),
+        child: ListView.separated(
+            separatorBuilder: ((context, index) {
+              return const Divider();
+            }),
+            itemCount: hotlines.length,
+            itemBuilder: (context, index) {
+              return Container(
+                child: HotlineButton(
+                    contactName: hotlines[index],
+                    contactNumber: numbers[index],
+                    onPressed: () async {
+                      var _text = 'tel:${numbers[index]}';
+                      if (await canLaunch(_text)) {
+                        await launch(_text);
+                      }
+                    }),
+              );
+            }),
       ),
     );
   }
